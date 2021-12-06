be0d85c42496b8d4be215707266c96fc562de555
xcr <itxcr@protonmail.com>
2021-12-06 10:47:08 +0800

    修改部分

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -10,13 +10,13 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
       config.plugin('html').tap((args) => {
-        args[0].title = 'xcr-pro'
+        args[0].title = '小超人聊天室'
         return args // [/* 传递给 html-webpack-plugin's 构造函数的新参数 */]
       })
     } else {
       // 为开发环境修改配置...
       config.plugin('html').tap((args) => {
-        args[0].title = 'xcr-dev'
+        args[0].title = '小超人聊天室'
         args[0].bodyName = 'xcr-Body'
         return args // [/* 传递给 html-webpack-plugin's 构造函数的新参数 */]
       })
