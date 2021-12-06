9d1797f17dafc308337b9c8d1afb244a1ed487c7
xcr <itxcr@protonmail.com>
2021-12-06 14:29:57 +0800

    更换背景

--- vue/src/config/index.ts
+++ vue/src/config/index.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_GROUP = '默认聊天室'
-export const DEFAULT_BACKGROUND = `https://itxcr.com:56999/public/imgs/wallhaven-rdjw2j_3840x2880.png`
+export const DEFAULT_BACKGROUND = `https://itxcr.com:56999/public/imgs/background.jpg`
