67d20ad715a78a956150489fe7f6133aa2b95308
xcr <itxcr@protonmail.com>
2021-12-23 19:57:32 +0800

    测试

--- vite/vite-test/vite.config.ts
+++ vite/vite-test/vite.config.ts
@@ -4,7 +4,7 @@ import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.ELECTRON == "true" ? "./" : ".",
+  // base: process.env.ELECTRON == "true" ? "./" : ".",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
