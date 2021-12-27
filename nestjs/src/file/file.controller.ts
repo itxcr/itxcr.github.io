51850b33b57cdb03a6d7e40adc54311055d41765
xcr <itxcr@protonmail.com>
2021-12-27 11:53:51 +0800

    改用pnpm

--- nestjs/src/file/file.controller.ts
+++ nestjs/src/file/file.controller.ts
@@ -5,12 +5,12 @@ import { MinioService } from '../minio/minio.service';
 @ApiTags('文件上传')
 @Controller('file')
 export class FileController {
-  constructor(private minioService: MinioService) {}
-
-  @Get('/get')
-  @ApiOperation({ summary: '获取文件', description: '获取文件' })
-  @ApiQuery({ name: 'id', description: '根据 id 获取指定文件', required: true })
-  async getFile(@Query() query) {
-    const data = await this.minioService.getFileUrls('blog', res);
-  }
+  // constructor(private minioService: MinioService) {}
+  //
+  // @Get('/get')
+  // @ApiOperation({ summary: '获取文件', description: '获取文件' })
+  // @ApiQuery({ name: 'id', description: '根据 id 获取指定文件', required: true })
+  // async getFile(@Query() query) {
+  //   // const data = await this.minioService.getFileUrls('blog', res);
+  // }
 }
--- nestjs/src/minio/minio.module.ts
+++ nestjs/src/minio/minio.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MinioController } from './minio.controller';
-import { MinioService } from './minio.service';
+// import { MinioService } from './minio.service';
 
 @Module({
   controllers: [MinioController],
-  providers: [MinioService],
+  // providers: [MinioService],
 })
 export class MinioModule {}
