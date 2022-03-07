bcfff818ae3a09526c1a9fffb41a900e598482fb
xcr <itxcr@protonmail.com>
2022-03-07 20:26:52 +0800

    继续ts学习

--- typescript/01.js
+++ typescript/01.js
@@ -1,21 +1,93 @@
-"use strict";
-exports.__esModule = true;
-var jQuery_1 = require("jQuery");
-var User = /** @class */ (function () {
-    function User() {
-    }
-    return User;
-}());
-console.log(jQuery_1["default"].ajax);
-function getUsers(cb) {
-    jQuery_1["default"].ajax({
-        url: 'api/users',
-        method: 'GET',
-        success: function (data) {
-            cb(data.items);
-        },
-        error: function (error) {
-            cb(null);
+// import $ from 'jQuery'
+//
+// class User {
+//   name: string;
+//   age: number;
+// }
+//
+// console.log($.ajax)
+//
+// function getUsers(cb: (users: User[]) => void): void {
+//   $.ajax({
+//     url: 'api/users',
+//     method: 'GET',
+//     success: function (data) {
+//       cb(data.items)
+//     },
+//     error: function (error) {
+//       cb(null)
+//     }
+//   })
+// }
+//
+// interface LoggerInterface {
+//   log(arg: any): void
+// }
+//
+// class Logger implements LoggerInterface {
+//   log(arg) {
+//     if (typeof console.log === 'function') {
+//       console.log(arg)
+//     } else {
+//       alert(arg)
+//     }
+//   }
+// }
+//
+// interface UserInterface {
+//   name: string,
+//   password: string
+// }
+//
+// let user: UserInterface = {
+//   name: '',
+//   password: '' // '{ name: string; pasword: string; }' is not assignable to type 'UserInterface'
+// }
+// namespace Geometry {
+//   interface VectorInterface {
+//   }
+//
+//   export interface Vector2dInterface {
+//   }
+//
+//   export interface Vector3dInterface {
+//   }
+//
+//   export class Vector2d implements VectorInterface, Vector2dInterface {
+//   }
+//
+//   export class Vector3d implements VectorInterface, Vector3dInterface {
+//   }
+// }
+// const vector2dInstance: Geometry.Vector2dInterface = new Geometry.Vector2d()
+// const vector3dInstance: Geometry.Vector3dInterface = new Geometry.Vector3d()
+var Geometry;
+(function (Geometry) {
+    var Vector2d = /** @class */ (function () {
+        function Vector2d(x, y) {
+            this._x = x;
+            this._y = y;
         }
-    });
-}
+        Vector2d.prototype.toArray = function (callback) {
+            callback([this._x, this._y]);
+        };
+        Vector2d.prototype.length = function () {
+            return Math.sqrt(this._x * this._x + this._y * this._y);
+        };
+        Vector2d.prototype.normalize = function () {
+            var len = 1 / this.length();
+            console.log(this.length());
+            console.log(len);
+            this._x *= len;
+            this._y *= len;
+            return { a: this._x, b: this._y };
+        };
+        return Vector2d;
+    }());
+    Geometry.Vector2d = Vector2d;
+})(Geometry || (Geometry = {}));
+var vector = new Geometry.Vector2d(2, 3);
+vector.normalize();
+vector.toArray(function (vectorAsArray) {
+    console.log(vectorAsArray);
+});
--- typescript/01.ts
+++ typescript/01.ts
@@ -1,20 +1,105 @@
-import $ from 'jQuery'
-class User {
-  name: string;
-  age: number;
-}
+// import $ from 'jQuery'
+//
+// class User {
+//   name: string;
+//   age: number;
+// }
+//
+// console.log($.ajax)
+//
+// function getUsers(cb: (users: User[]) => void): void {
+//   $.ajax({
+//     url: 'api/users',
+//     method: 'GET',
+//     success: function (data) {
+//       cb(data.items)
+//     },
+//     error: function (error) {
+//       cb(null)
+//     }
+//   })
+// }
+//
+// interface LoggerInterface {
+//   log(arg: any): void
+// }
+//
+// class Logger implements LoggerInterface {
+//   log(arg) {
+//     if (typeof console.log === 'function') {
+//       console.log(arg)
+//     } else {
+//       alert(arg)
+//     }
+//   }
+// }
+//
+// interface UserInterface {
+//   name: string,
+//   password: string
+// }
+//
+// let user: UserInterface = {
+//   name: '',
+//   password: '' // '{ name: string; pasword: string; }' is not assignable to type 'UserInterface'
+// }
 
-console.log($.ajax)
+// namespace Geometry {
+//   interface VectorInterface {
+//   }
+//
+//   export interface Vector2dInterface {
+//   }
+//
+//   export interface Vector3dInterface {
+//   }
+//
+//   export class Vector2d implements VectorInterface, Vector2dInterface {
+//   }
+//
+//   export class Vector3d implements VectorInterface, Vector3dInterface {
+//   }
+// }
+// const vector2dInstance: Geometry.Vector2dInterface = new Geometry.Vector2d()
+// const vector3dInstance: Geometry.Vector3dInterface = new Geometry.Vector3d()
 
-function getUsers(cb: (users: User[]) => void): void {
-  $.ajax({
-    url: 'api/users',
-    method: 'GET',
-    success: function (data) {
-      cb(data.items)
-    },
-    error: function (error) {
-      cb(null)
+module Geometry {
+  export interface Vector2dInterface {
+    toArray(callback: (x: number[]) => void): void
+
+    length(): number
+
+    normalize()
+  }
+
+  export class Vector2d implements Vector2dInterface {
+    private _x: number
+    private _y: number
+
+    constructor(x: number, y: number) {
+      this._x = x
+      this._y = y
     }
-  })
+
+    toArray(callback: (x: number[]) => void): void {
+      callback([this._x, this._y])
+    }
+
+    length(): number {
+      return Math.sqrt(this._x * this._x + this._y * this._y)
+    }
+
+    normalize() {
+      let len = 1 / this.length()
+      this._x *= len
+      this._y *= len
+      return {a: this._x, b: this._y}
+    }
+  }
 }
+
+const vector: Geometry.Vector2dInterface = new Geometry.Vector2d(2, 3)
+vector.normalize()
+vector.toArray((vectorAsArray: number[]) => {
+  console.log(vectorAsArray)
+})
