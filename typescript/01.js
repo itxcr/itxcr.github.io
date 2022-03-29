99cdf2d6d6d05fa14d04a9cbdff2646c9855f348
xcr <itxcr@protonmail.com>
2022-03-29 20:27:48 +0800

    继续学习TS

--- typescript/01.js
+++ typescript/01.js
@@ -1,246 +1,72 @@
-// import $ from 'jQuery'
-//
-// class User {
-//   name: string;
-//   age: number;
-// }
-//
-// console.log($.ajax)
-//
-// function getUsers(cb: (users: User[]) => void): void {
-//   $.ajax({
-//     url: 'api/users',
-//     method: 'GET',
-//     success: function (data) {
-//       cb(data.items)
-//     },
-//     error: function (error) {
-//       cb(null)
-//     }
-//   })
-// }
-//
-// interface LoggerInterface {
-//   log(arg: any): void
-// }
-//
-// class Logger implements LoggerInterface {
-//   log(arg) {
-//     if (typeof console.log === 'function') {
-//       console.log(arg)
-//     } else {
-//       alert(arg)
-//     }
-//   }
-// }
-//
-// interface UserInterface {
-//   name: string,
-//   password: string
-// }
-//
-// let user: UserInterface = {
-//   name: '',
-//   password: '' // '{ name: string; pasword: string; }' is not assignable to type 'UserInterface'
-// }
-// namespace Geometry {
-//   interface VectorInterface {
-//   }
-//
-//   export interface Vector2dInterface {
-//   }
-//
-//   export interface Vector3dInterface {
-//   }
-//
-//   export class Vector2d implements VectorInterface, Vector2dInterface {
-//   }
-//
-//   export class Vector3d implements VectorInterface, Vector3dInterface {
-//   }
-// }
-// const vector2dInstance: Geometry.Vector2dInterface = new Geometry.Vector2d()
-// const vector3dInstance: Geometry.Vector3dInterface = new Geometry.Vector3d()
-// module Geometry {
-//   export interface Vector2dInterface {
-//     toArray(callback: (x: number[]) => void): void
-//
-//     length(): number
-//
-//     normalize()
-//   }
-//
-//   export class Vector2d implements Vector2dInterface {
-//     private _x: number
-//     private _y: number
-//
-//     constructor(x: number, y: number) {
-//       this._x = x
-//       this._y = y
-//     }
-//
-//     toArray(callback: (x: number[]) => void): void {
-//       callback([this._x, this._y])
-//     }
-//
-//     length(): number {
-//       return Math.sqrt(this._x * this._x + this._y * this._y)
-//     }
-//
-//     normalize() {
-//       let len = 1 / this.length()
-//       this._x *= len
-//       this._y *= len
-//       return {a: this._x, b: this._y}
-//     }
-//   }
-// }
-//
-// const vector: Geometry.Vector2dInterface = new Geometry.Vector2d(2, 3)
-// vector.normalize()
-// vector.toArray((vectorAsArray: number[]) => {
-//   console.log(vectorAsArray)
-// })
-// function greetNamed(name: string):string {
-//   if (name) {
-//     return `Hi! ${name}`
-//   }
-// }
-// let greetUnnamed:(name: string) => string
-// greetUnnamed = function (name: string): string {
-//   if (name) {
-//     return `Hi! ${name}`
-//   }
-// }
-// let greedUnnamed:(name: string) => string = function (name: string):string {
-//   if (name) {
-//     return `Hi! ${name}`
-//   }
-// }
-// function add(foo: number, bar: number, foobar: number = 0): number {
-//   return foo + bar + foobar
-// }
-// function add(foo, bar, foobar) {
-//   if (foobar === void 0) {
-//     foobar = 0
-//   }
-//   return foo + bar + foobar
-// }
-// function add(...foo:number[]):number {
-//   let result = 0
-//   for (let i = 0, j = foo.length; i < j; i++) {
-//     result += foo[i]
-//   }
-//   return result
-// }
-//
-// console.log(add())
-// add(2, 3)
-// function add() {
-//   let foo = []
-//   for (let i = 0, j = arguments.length; i < j; i++) {
-//     foo[i - 0] = arguments[i]
-//   }
-//   let result = 0
-//   for (let i = 0, j = foo.length; i < j; i++) {
-//     result += foo[i]
-//   }
-//   return result
-// }
-//
-// // @ts-ignore
-// console.log(add(1, 2, 3))
-// function add(foo: number[]):number {
-//   let result = 0
-//   for (let i =0, j = foo.length; i < j; i++) {
-//     result += foo[i]
-//   }
-//   return result
-// }
-//
-// console.log(add([]))
-// console.log(add(2))
-// console.log(add(2, 2))
-// console.log(add(2, 2, 2))
-// function test(name: string):string    // 重载签名
-// function test(age: number): string    // 重载签名
-// function test(single: boolean):string // 重载签名
-// function test(value: string | number | boolean): string { // 实现签名
-//   switch (typeof value) {
-//     case 'string':
-//       return `My name is ${value}`
-//     case 'number':
-//       return `Im ${value} years old`
-//     case 'boolean':
-//       return value ? `I'm single` : `I'm not single`
-//     default:
-//       console.log('Invalid Operation!')
-//   }
-// }
-// interface Document {
-//   createElement(tagName: 'div'):HTMLDivElement
-//   createElement(tagName: 'span'):HTMLSpanElement
-//   createElement(tagName: 'canvas'):HTMLCanvasElement
-//   createElement(tagName: string): HTMLElement
-// }
-// function foo2():void {
-//   bar = 0
-//   var bar: number
-//   alert(bar)
-// }
-// foo2()
-// var bar = 0; // 全局的
-// (function (global) {
-//     var foo: number = 0 // 在函数作用域中
-//     bar = 1  // 在全局作用域中
-//     console.log(global.bar) // 1
-//     console.log(foo) // 0
-//   })(this)
-//
-// console.log(bar) // 1
-// console.log(foo) // 错误
-// class Counter {
-//   private _i:number
-//   constructor() {
-//     this._i = 0
-//   }
-//   get():number {
-//     return this._i
-//   }
-//   set(val: number): void {
-//     this._i = val
-//   }
-//   increment(): void {
-//     this._i ++
-//   }
-// }
-//
-// var counter = new Counter()
-// console.log(counter.get()) // 0
-// counter.set(2)
-// console.log(counter.get()) // 2
-// counter.increment()
-// console.log(counter.get()) // 3
-// console.log(counter._i) // 错误 _i 为私有属性
-var Counter = (function () {
-    var _i = 0;
-    function Counter() {
-    }
-    Counter.prototype.get = function () {
-        return _i;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-    Counter.prototype.set = function (val) {
-        _i = val;
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    Counter.prototype.increment = function () {
-        _i++;
-    };
-    return Counter;
 })();
-var a = new Counter();
-console.log(a.get());
-a.set(2);
-console.log(a.get());
-a.increment();
-console.log(a.get());
-console.log(a._i);
+var Email = /** @class */ (function () {
+    function Email(email) {
+        if (this.validateEmail(email)) {
+            this.email = email;
+        }
+        else {
+            throw new Error('Invalid email');
+        }
+    }
+    Email.prototype.validateEmail = function (email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    };
+    Email.prototype.get = function () {
+        return this.email;
+    };
+    return Email;
+}());
+var Person = /** @class */ (function () {
+    function Person(name, surname, email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+    Person.prototype.greet = function () {
+        console.log('Hello');
+    };
+    return Person;
+}());
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(name, surname, email, subjects) {
+        var _this = _super.call(this, name, surname, email) || this;
+        _this.subjects = subjects;
+        return _this;
+    }
+    Teacher.prototype.greet = function () {
+        _super.prototype.greet.call(this);
+        console.log('开示教书' + this.subjects);
+    };
+    Teacher.prototype.teach = function () {
+        console.log('教书');
+    };
+    return Teacher;
+}(Person));
+var SchoolPrincipal = /** @class */ (function (_super) {
+    __extends(SchoolPrincipal, _super);
+    function SchoolPrincipal() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    SchoolPrincipal.prototype.manageTeachers = function () {
+        console.log('我们需要帮助学生变得更好');
+    };
+    return SchoolPrincipal;
+}(Teacher));
+var principal = new SchoolPrincipal('testP', 'P', new Email('p@p.com'), ['数学', '物理']);
+principal.greet();
+principal.teach();
+principal.manageTeachers();
--- typescript/01.ts
+++ typescript/01.ts
@@ -1,277 +1,63 @@
-// import $ from 'jQuery'
-//
-// class User {
-//   name: string;
-//   age: number;
-// }
-//
-// console.log($.ajax)
-//
-// function getUsers(cb: (users: User[]) => void): void {
-//   $.ajax({
-//     url: 'api/users',
-//     method: 'GET',
-//     success: function (data) {
-//       cb(data.items)
-//     },
-//     error: function (error) {
-//       cb(null)
-//     }
-//   })
-// }
-//
-// interface LoggerInterface {
-//   log(arg: any): void
-// }
-//
-// class Logger implements LoggerInterface {
-//   log(arg) {
-//     if (typeof console.log === 'function') {
-//       console.log(arg)
-//     } else {
-//       alert(arg)
-//     }
-//   }
-// }
-//
-// interface UserInterface {
-//   name: string,
-//   password: string
-// }
-//
-// let user: UserInterface = {
-//   name: '',
-//   password: '' // '{ name: string; pasword: string; }' is not assignable to type 'UserInterface'
-// }
+class Email {
+  private readonly email: string
 
-// namespace Geometry {
-//   interface VectorInterface {
-//   }
-//
-//   export interface Vector2dInterface {
-//   }
-//
-//   export interface Vector3dInterface {
-//   }
-//
-//   export class Vector2d implements VectorInterface, Vector2dInterface {
-//   }
-//
-//   export class Vector3d implements VectorInterface, Vector3dInterface {
-//   }
-// }
-// const vector2dInstance: Geometry.Vector2dInterface = new Geometry.Vector2d()
-// const vector3dInstance: Geometry.Vector3dInterface = new Geometry.Vector3d()
+  constructor(email: string) {
+    if (this.validateEmail(email)) {
+      this.email = email
+    } else {
+      throw new Error('Invalid email')
+    }
+  }
 
-// module Geometry {
-//   export interface Vector2dInterface {
-//     toArray(callback: (x: number[]) => void): void
-//
-//     length(): number
-//
-//     normalize()
-//   }
-//
-//   export class Vector2d implements Vector2dInterface {
-//     private _x: number
-//     private _y: number
-//
-//     constructor(x: number, y: number) {
-//       this._x = x
-//       this._y = y
-//     }
-//
-//     toArray(callback: (x: number[]) => void): void {
-//       callback([this._x, this._y])
-//     }
-//
-//     length(): number {
-//       return Math.sqrt(this._x * this._x + this._y * this._y)
-//     }
-//
-//     normalize() {
-//       let len = 1 / this.length()
-//       this._x *= len
-//       this._y *= len
-//       return {a: this._x, b: this._y}
-//     }
-//   }
-// }
-//
-// const vector: Geometry.Vector2dInterface = new Geometry.Vector2d(2, 3)
-// vector.normalize()
-// vector.toArray((vectorAsArray: number[]) => {
-//   console.log(vectorAsArray)
-// })
+  private validateEmail(email: string) {
+    let re = /\S+@\S+\.\S+/
+    return re.test(email)
+  }
 
-// function greetNamed(name: string):string {
-//   if (name) {
-//     return `Hi! ${name}`
-//   }
-// }
-
-
-// let greetUnnamed:(name: string) => string
-// greetUnnamed = function (name: string): string {
-//   if (name) {
-//     return `Hi! ${name}`
-//   }
-// }
-
-// let greedUnnamed:(name: string) => string = function (name: string):string {
-//   if (name) {
-//     return `Hi! ${name}`
-//   }
-// }
-
-// function add(foo: number, bar: number, foobar: number = 0): number {
-//   return foo + bar + foobar
-// }
-
-// function add(foo, bar, foobar) {
-//   if (foobar === void 0) {
-//     foobar = 0
-//   }
-//   return foo + bar + foobar
-// }
-
-// function add(...foo:number[]):number {
-//   let result = 0
-//   for (let i = 0, j = foo.length; i < j; i++) {
-//     result += foo[i]
-//   }
-//   return result
-// }
-//
-// console.log(add())
-
-// add(2, 3)
-
-// function add() {
-//   let foo = []
-//   for (let i = 0, j = arguments.length; i < j; i++) {
-//     foo[i - 0] = arguments[i]
-//   }
-//   let result = 0
-//   for (let i = 0, j = foo.length; i < j; i++) {
-//     result += foo[i]
-//   }
-//   return result
-// }
-//
-// // @ts-ignore
-// console.log(add(1, 2, 3))
-
-// function add(foo: number[]):number {
-//   let result = 0
-//   for (let i =0, j = foo.length; i < j; i++) {
-//     result += foo[i]
-//   }
-//   return result
-// }
-//
-// console.log(add([]))
-// console.log(add(2))
-// console.log(add(2, 2))
-// console.log(add(2, 2, 2))
-
-// function test(name: string):string    // 重载签名
-// function test(age: number): string    // 重载签名
-// function test(single: boolean):string // 重载签名
-// function test(value: string | number | boolean): string { // 实现签名
-//   switch (typeof value) {
-//     case 'string':
-//       return `My name is ${value}`
-//     case 'number':
-//       return `Im ${value} years old`
-//     case 'boolean':
-//       return value ? `I'm single` : `I'm not single`
-//     default:
-//       console.log('Invalid Operation!')
-//   }
-// }
-
-// interface Document {
-//   createElement(tagName: 'div'):HTMLDivElement
-//   createElement(tagName: 'span'):HTMLSpanElement
-//   createElement(tagName: 'canvas'):HTMLCanvasElement
-//   createElement(tagName: string): HTMLElement
-// }
-
-// function foo2():void {
-//   bar = 0
-//   var bar: number
-//   alert(bar)
-// }
-// foo2()
-
-// var bar = 0; // 全局的
-// (function (global) {
-//     var foo: number = 0 // 在函数作用域中
-//     bar = 1  // 在全局作用域中
-//     console.log(global.bar) // 1
-//     console.log(foo) // 0
-//   })(this)
-//
-// console.log(bar) // 1
-// console.log(foo) // 错误
-
-// class Counter {
-//   private _i:number
-//   constructor() {
-//     this._i = 0
-//   }
-//   get():number {
-//     return this._i
-//   }
-//   set(val: number): void {
-//     this._i = val
-//   }
-//   increment(): void {
-//     this._i ++
-//   }
-// }
-//
-// var counter = new Counter()
-// console.log(counter.get()) // 0
-// counter.set(2)
-// console.log(counter.get()) // 2
-// counter.increment()
-// console.log(counter.get()) // 3
-// console.log(counter._i) // 错误 _i 为私有属性
-
-
-// var Counter = (function () {
-//   var _i:number = 0
-//   function Counter() {
-//
-//   }
-//   Counter.prototype.get = function (){
-//     return _i
-//   }
-//   Counter.prototype.set = function (val: number){
-//     _i = val
-//   }
-//   Counter.prototype.increment = function () {
-//     _i++
-//   }
-//   return Counter
-// })()
-//
-// var a = new Counter()
-// console.log(a.get())
-// a.set(2)
-// console.log(a.get())
-// a.increment()
-// console.log(a.get())
-// console.log(a._i)
-class User {
-  name: string
-  age: number
+  get():string {
+    return this.email
+  }
+}
+class Person {
+  public name: string
+  public surname: string
+  public email: Email
+  constructor(name: string, surname: string, email: Email) {
+    this.name = name
+    this.surname = surname
+    this.email = email
+  }
+  greet() {
+    console.log('Hello')
+  }
 }
 
-function getEntities<T>(url: string, cb: (list : T[]) => void):void {
+class Teacher extends Person {
+  public subjects: string[]
+  constructor(name: string, surname: string, email: Email, subjects: string[]) {
+    super(name, surname, email);
+    this.subjects = subjects
+  }
 
+  greet() {
+    super.greet()
+    console.log('开示教书' + this.subjects)
+  }
+  teach() {
+    console.log('教书')
+  }
 }
 
-getEntities()<User>
+class SchoolPrincipal extends Teacher {
+  manageTeachers() {
+    console.log('我们需要帮助学生变得更好')
+  }
+}
+
+let principal = new SchoolPrincipal('testP', 'P', new Email('p@p.com'), ['数学', '物理'])
+principal.greet()
+principal.teach()
+principal.manageTeachers()
+
+
+
