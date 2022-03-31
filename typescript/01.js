a6c53e205cd10c1dd5b575d49e0787b8f8ae1d45
xcr <itxcr@protonmail.com>
2022-03-31 20:49:04 +0800

    继续学习ts

--- typescript/01.js
+++ typescript/01.js
@@ -1,72 +1,102 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Email = /** @class */ (function () {
-    function Email(email) {
-        if (this.validateEmail(email)) {
-            this.email = email;
-        }
-        else {
-            throw new Error('Invalid email');
-        }
+// class Email {
+//   private readonly email: string
+//
+//   constructor(email: string) {
+//     if (this.validateEmail(email)) {
+//       this.email = email
+//     } else {
+//       throw new Error('Invalid email')
+//     }
+//   }
+//
+//   private validateEmail(email: string) {
+//     let re = /\S+@\S+\.\S+/
+//     return re.test(email)
+//   }
+//
+//   get():string {
+//     return this.email
+//   }
+// }
+// class Person {
+//   public name: string
+//   public surname: string
+//   public email: Email
+//   constructor(name: string, surname: string, email: Email) {
+//     this.name = name
+//     this.surname = surname
+//     this.email = email
+//   }
+//   greet() {
+//     console.log('Hello')
+//   }
+// }
+//
+// class Teacher extends Person {
+//   public subjects: string[]
+//   constructor(name: string, surname: string, email: Email, subjects: string[]) {
+//     super(name, surname, email);
+//     this.subjects = subjects
+//   }
+//
+//   greet() {
+//     super.greet()
+//     console.log('开示教书' + this.subjects)
+//   }
+//   teach() {
+//     console.log('教书')
+//   }
+// }
+//
+// class SchoolPrincipal extends Teacher {
+//   manageTeachers() {
+//     console.log('我们需要帮助学生变得更好')
+//   }
+// }
+//
+// let principal = new SchoolPrincipal('testP', 'P', new Email('p@p.com'), ['数学', '物理'])
+// principal.greet()
+// principal.teach()
+// principal.manageTeachers()
+var Animal = /** @class */ (function () {
+    function Animal() {
     }
-    Email.prototype.validateEmail = function (email) {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
+    Animal.prototype.eat = function () {
+        console.log('eat');
     };
-    Email.prototype.get = function () {
-        return this.email;
-    };
-    return Email;
+    return Animal;
 }());
-var Person = /** @class */ (function () {
-    function Person(name, surname, email) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
+var Mammal = /** @class */ (function () {
+    function Mammal() {
     }
-    Person.prototype.greet = function () {
-        console.log('Hello');
+    Mammal.prototype.breathe = function () {
+        console.log('breathe');
     };
-    return Person;
+    return Mammal;
 }());
-var Teacher = /** @class */ (function (_super) {
-    __extends(Teacher, _super);
-    function Teacher(name, surname, email, subjects) {
-        var _this = _super.call(this, name, surname, email) || this;
-        _this.subjects = subjects;
-        return _this;
+var WingedAnimal = /** @class */ (function () {
+    function WingedAnimal() {
     }
-    Teacher.prototype.greet = function () {
-        _super.prototype.greet.call(this);
-        console.log('开示教书' + this.subjects);
+    WingedAnimal.prototype.fly = function () {
+        console.log('fly');
     };
-    Teacher.prototype.teach = function () {
-        console.log('教书');
-    };
-    return Teacher;
-}(Person));
-var SchoolPrincipal = /** @class */ (function (_super) {
-    __extends(SchoolPrincipal, _super);
-    function SchoolPrincipal() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    return WingedAnimal;
+}());
+var Bat = /** @class */ (function () {
+    function Bat() {
     }
-    SchoolPrincipal.prototype.manageTeachers = function () {
-        console.log('我们需要帮助学生变得更好');
-    };
-    return SchoolPrincipal;
-}(Teacher));
-var principal = new SchoolPrincipal('testP', 'P', new Email('p@p.com'), ['数学', '物理']);
-principal.greet();
-principal.teach();
-principal.manageTeachers();
+    return Bat;
+}());
+function applyMixins(derivedCtor, baseCtors) {
+    baseCtors.forEach(function (baseCtor) {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(function (name) {
+            if (name !== 'constructor') {
+                derivedCtor.prototype[name] = baseCtor.prototype[name];
+            }
+        });
+    });
+}
+applyMixins(Bat, [Mammal, WingedAnimal]);
+var a = new Bat();
+a.fly();
+a.breathe();
