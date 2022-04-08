8ca533ed967793332abeb38d996c655076629d1e
xcr <itxcr@protonmail.com>
2022-04-09 06:28:04 +0800

    命名空间

--- typescript/01.ts
+++ typescript/01.ts
@@ -1,16 +1,5 @@
-class User {
-    public name: string
-    public password: string
-}
-
-class NotGenericUserRepository<T> {
-    private _url: string
-
-    constructor(url: string) {
-        this._url = url
-    }
-
-    public getAsync() {
-        return Q.Promise((resolve: (users: User[]) =>), resect)
-    }
+namespace app {
+  export class UserModel {
+    // ...
+  }
 }
