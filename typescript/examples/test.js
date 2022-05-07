5bf80e8fb1ffba1d0d717b448845449a932e4069
xcr <itxcr@protonmail.com>
2022-05-07 18:12:57 +0800

    装饰器 反射元数据 API

--- typescript/examples/test.js
+++ typescript/examples/test.js
@@ -1,32 +1,163 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Base = /** @class */ (function () {
-    function Base() {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var Person = /** @class */ (function () {
+    function Person(name, surname) {
+        this.name = name;
+        this.surname = surname;
     }
-    Base.prototype.method1 = function () { return 1; };
-    Base.prototype.method2 = function () { return 2; };
-    return Base;
+    Person.prototype.saySomething = function (something) {
+        return this.name + '' + this.surname + 'says:' + something;
+    };
+    Person = __decorate([
+        logClass("option")
+        // @ts-ignore
+    ], Person);
+    return Person;
 }());
-var Derived = /** @class */ (function (_super) {
-    __extends(Derived, _super);
-    function Derived() {
-        return _super !== null && _super.apply(this, arguments) || this;
+function logClass(option) {
+    return function (target) {
+        var original = target;
+        // 用来生成类的实例的工具方法
+        function construct(constructor, args) {
+            var c = function () {
+                return constructor.apply(this, args);
+            };
+            c.prototype = constructor.prototype;
+            return new c();
+        }
+        // 新的构造函数行为
+        var f = function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            console.log("New:" + original.name);
+            args.push('1231312');
+            return construct(original, args);
+        };
+        // 复制原型,保证 instanceof 操作能正常使用
+        f.prototype = original.prototype;
+        // 返回新的构造函数(会覆盖原构造函数)
+        console.log(option, target);
+        return f;
+    };
+}
+function logMethod(target, key, descriptor) {
+    // 保存原方法的引用
+    console.log(descriptor);
+    var originMethod = descriptor.value;
+    // 编辑 descriptor 参数的 value 属性
+    descriptor.value = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        // 将方法参数转换成字符串
+        var a = args.map(function (a) { return JSON.stringify(a); }).join();
+        // 执行方法 得道其返回值
+        var result = originMethod.apply(this, args);
+        // 将返回值转化为字符串
+        var r = JSON.stringify(result);
+        // 将函数调用细节打印到控制台
+        console.log("Call: ".concat(key, "(").concat(a, ") => ").concat(r));
+        return result;
+    };
+    // 返回编辑后的属性描述对象
+    return descriptor;
+}
+function logProperty(target, key) {
+    // 属性值
+    var _val = this[key];
+    // 属性的 getter
+    var getter = function () {
+        console.log("Get:".concat(key, "=>").concat(_val));
+        return _val;
+    };
+    // 属性的 setter
+    var setter = function (newVal) {
+        console.log("Set:".concat(key, "=>").concat(newVal));
+        _val = newVal;
+    };
+    // 删除属性，在严格模式下，如果对象是不可配置的，删除操作符将会抛出一个错误
+    // 在非严格模式下，则会返回 false
+    if (delete this[key]) {
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+            enumerable: true,
+            configurable: true
+        });
     }
-    Derived.prototype.method2 = function () { return 3; };
-    Derived.prototype.method3 = function () { return 4; };
-    return Derived;
-}(Base));
-console.log(Base.prototype);
+}
+function addMetadata(target, key, index) {
+    var metadataKey = "_log_".concat(key, "_parameters");
+    if (Array.isArray(target[metadataKey])) {
+        target[metadataKey].push(index);
+    }
+    else {
+        target[metadataKey] = [index];
+    }
+    console.log(target);
+}
+function logParameter(target, key, index) {
+    var metadataKey = "_log_".concat(key, "_parameters");
+    if (Array.isArray(target[metadataKey])) {
+        target[metadataKey].push(index);
+    }
+    else {
+        target[metadataKey] = [index];
+    }
+    console.log(target);
+}
+function readMetadata(target, key, descriptor) {
+    var originalMethod = descriptor.value;
+    descriptor.value = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        var metadataKey = "_log_".concat(key, "_parameters");
+        var includes = target[metadataKey];
+        if (Array.isArray(includes)) {
+            for (var i = 0; i < args.length; i++) {
+                if (includes.indexOf(i) !== -1) {
+                    var arg = args[i];
+                    var argStr = JSON.stringify(arg) || arg.toString();
+                    console.log("".concat(key, " arg[").concat(i, "]:").concat(argStr));
+                }
+            }
+            var result = originalMethod.apply(this, args);
+            return result;
+        }
+    };
+    return descriptor;
+}
+function log() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    switch (args.length) {
+        case 1:
+            return logClass.apply(this, args);
+        case 2:
+            // 由于属性装饰器没有返回值
+            // 使用 break 取代return
+            logProperty.apply(this, args);
+            break;
+        case 3:
+            if (typeof args[2] === 'number') {
+                logParameter.apply(this, args);
+            }
+            return logMethod.apply(this, args);
+        default:
+            throw new Error("装饰器不符合规范");
+    }
+}
+var me = new Person('xcr', '666');
+console.log(Person.prototype);
+me.saySomething('hello');
--- typescript/examples/test.ts
+++ typescript/examples/test.ts
@@ -1,10 +1,151 @@
-class Base {
-    public method1() {return 1}
-    public method2() {return 2}
-}
-class Derived extends Base {
-    public method2() {return 3}
-    public method3() {return 4}
+@logClass("option")
+// @ts-ignore
+class Person {
+    public name: string
+    public surname: string
+    constructor(name: string, surname: string) {
+        this.name = name
+        this.surname = surname
+    }
+    public saySomething(something: string): string {
+        return this.name + '' + this.surname + 'says:' + something
+    }
 }
 
-console.log(Base.prototype)
+
+function logClass(option: string) {
+    return function (target: any) {
+        const original = target
+        // 用来生成类的实例的工具方法
+        function construct(constructor, args) {
+            const c: any = function () {
+                return constructor.apply(this, args)
+            }
+            c.prototype = constructor.prototype
+            return new c()
+        }
+
+        // 新的构造函数行为
+        const f: any = function (...args) {
+            console.log("New:" + original.name)
+            args.push('1231312')
+            return construct(original, args)
+        }
+        // 复制原型,保证 instanceof 操作能正常使用
+        f.prototype = original.prototype
+        // 返回新的构造函数(会覆盖原构造函数)
+        console.log(option, target)
+        return f
+    }
+}
+
+
+function logMethod(target: any, key: string, descriptor: any) {
+    // 保存原方法的引用
+    console.log(descriptor)
+    const originMethod = descriptor.value
+    // 编辑 descriptor 参数的 value 属性
+    descriptor.value = function (...args: any[]) {
+        // 将方法参数转换成字符串
+        const a = args.map(a => JSON.stringify(a)).join()
+        // 执行方法 得道其返回值
+        const result = originMethod.apply(this, args)
+        // 将返回值转化为字符串
+        const r = JSON.stringify(result)
+        // 将函数调用细节打印到控制台
+        console.log(`Call: ${key}(${a}) => ${r}`)
+        return result
+    }
+    // 返回编辑后的属性描述对象
+    return descriptor
+}
+
+function logProperty(target: any, key: string) {
+    // 属性值
+    let _val = this[key]
+    // 属性的 getter
+    const getter = function () {
+        console.log(`Get:${key}=>${_val}`)
+        return _val
+    }
+    // 属性的 setter
+    const setter = function (newVal) {
+        console.log(`Set:${key}=>${newVal}`)
+        _val = newVal
+    }
+    // 删除属性，在严格模式下，如果对象是不可配置的，删除操作符将会抛出一个错误
+    // 在非严格模式下，则会返回 false
+    if (delete this[key]) {
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+            enumerable: true,
+            configurable: true
+        })
+    }
+}
+
+function addMetadata(target: any, key: string, index: number) {
+    const metadataKey = `_log_${key}_parameters`
+    if (Array.isArray(target[metadataKey])) {
+        target[metadataKey].push(index)
+    } else {
+        target[metadataKey] = [index]
+    }
+    console.log(target)
+}
+
+function logParameter(target: any, key: string, index: number) {
+    const metadataKey = `_log_${key}_parameters`
+    if (Array.isArray(target[metadataKey])) {
+        target[metadataKey].push(index)
+    } else {
+        target[metadataKey] = [index]
+    }
+    console.log(target)
+}
+
+function readMetadata(target: any, key: string, descriptor: any) {
+    const originalMethod = descriptor.value
+    descriptor.value = function (...args: any[]) {
+        const metadataKey = `_log_${key}_parameters`
+        const includes = target[metadataKey]
+        if (Array.isArray(includes)) {
+            for (let i = 0; i < args.length; i++) {
+                if (includes.indexOf(i) !== -1) {
+                    const arg = args[i]
+                    const argStr = JSON.stringify(arg) || arg.toString()
+                    console.log(`${key} arg[${i}]:${argStr}`)
+                }
+            }
+            const result = originalMethod.apply(this, args)
+            return result
+        }
+    }
+    return descriptor
+}
+
+function log(...args: any[]) {
+    switch (args.length) {
+        case 1:
+            return logClass.apply(this, args)
+        case 2:
+            // 由于属性装饰器没有返回值
+            // 使用 break 取代return
+            logProperty.apply(this, args)
+            break
+        case 3:
+            if (typeof args[2] === 'number') {
+                logParameter.apply(this, args)
+            }
+            return logMethod.apply(this, args)
+        default:
+            throw new Error("装饰器不符合规范")
+    }
+}
+
+
+const me = new Person('xcr', '666')
+console.log(Person.prototype)
+
+me.saySomething('hello')
