60369bfa861c42d35cd32a853f1292e6a8153775
xcr <itxcr@protonmail.com>
2023-03-20 21:41:26 +0800

    feat: 下一节 3-14

--- typescript/AllInOne/examples/2-深入/11.ts
+++ typescript/AllInOne/examples/2-深入/11.ts
@@ -19,23 +19,47 @@ class People {
     }
 }
 
-// 每次调用 Object.getOwnPropertyDescriptor 都会开辟新的内存空间
-const getProp = Object.getOwnPropertyDescriptor(People.prototype, 'doEat')
-console.log(getProp, 'getProp')
-const targetMethod = getProp!.value
-getProp!.value = function (...args: any[]) {
-    args = args.map(v => {
-        if (typeof v === 'string') return StringUtil.trimSpace(v)
-        return v
-    })
-    console.log('前置')
-    console.log(this)
-    targetMethod.apply(this, args)
-    console.log('后置')
-}
+function intercept(targetClass: Function) {
+    const getProp = Object.getOwnPropertyDescriptor(targetClass.prototype, 'doEat')
+    console.log(getProp, 'getProp')
+    const targetMethod = getProp!.value
+    getProp!.value = function (...args: any[]) {
+        args = args.map(v => {
+            if (typeof v === 'string') return StringUtil.trimSpace(v)
+            return v
+        })
+        console.log('前置')
+        console.log(this)
+        targetMethod.apply(this, args)
+        console.log('后置')
+    }
 
 // getProp!.value('xcr', 'test')
-Object.defineProperty(People.prototype, 'doEat', getProp!)
+    Object.defineProperty(targetClass.prototype, 'doEat', getProp!)
+}
+
+
+// 每次调用 Object.getOwnPropertyDescriptor 都会开辟新的内存空间
+// const getProp = Object.getOwnPropertyDescriptor(People.prototype, 'doEat')
+// console.log(getProp, 'getProp')
+// const targetMethod = getProp!.value
+// getProp!.value = function (...args: any[]) {
+//     args = args.map(v => {
+//         if (typeof v === 'string') return StringUtil.trimSpace(v)
+//         return v
+//     })
+//     console.log('前置')
+//     console.log(this)
+//     targetMethod.apply(this, args)
+//     console.log('后置')
+// }
+
+// getProp!.value('xcr', 'test')
+// Object.defineProperty(People.prototype, 'doEat', getProp!)
+
+// 拦截器封装
+intercept(People)
+
 const p = new People('xcr', 'test')
 p.doEat('x  x  x', 'p  p  p')
 
