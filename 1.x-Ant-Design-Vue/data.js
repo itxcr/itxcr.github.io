abd3c58d2f12d294e7a868f3d36b525e14acf3cd
xcr <itxcr@protonmail.com>
2023-03-23 17:23:37 +0800

    feat: 树

---
+++ 1.x-Ant-Design-Vue/data.js
@@ -0,0 +1,65 @@
+const data = {
+  treeData: [
+    {
+      id: 1,
+      parentId: 0,
+      key: '1',
+      title: '工作台',
+    },
+    {
+      id: 2,
+      parentId: 0,
+      key: '2',
+      title: '仪表盘',
+      children: [
+        {
+          id: 3,
+          parentId: 2,
+          key: '3',
+          title: '欢迎页',
+        },
+        {
+          id: 4,
+          parentId: 2,
+          key: '4',
+          title: '分析页',
+        },
+        {
+          id: 5,
+          parentId: 2,
+          key: '5',
+          title: '监控页',
+        },
+      ],
+    },
+    {
+      id: 6,
+      parentId: 0,
+      key: '6',
+      title: '表单页',
+      children: [
+        {
+          id: 7,
+          parentId: 6,
+          key: '7',
+          title: '基础表单',
+        },
+        {
+          id: 8,
+          parentId: 6,
+          key: '8',
+          title: '分步表单',
+        },
+        {
+          id: 9,
+          parentId: 6,
+          key: '9',
+          title: '高级表单',
+        },
+      ],
+    },
+  ],
+  editKey: [],
+}
+
+export default data
