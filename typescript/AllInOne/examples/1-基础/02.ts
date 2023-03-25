bbb7582c38493eeba388a0eba79a9093fab5bfc1
xcr <itxcr@protonmail.com>
2023-03-25 18:06:55 +0800

    feat: tsconfig 简单学习

--- typescript/AllInOne/examples/1-基础/02.ts
+++ typescript/AllInOne/examples/1-基础/02.ts
@@ -4,4 +4,7 @@ let money=30
 let string='30'
 
 let a = 123
-export {}
+export default function (a: number, b: number): number {
+    return a + b
+
+}
--- typescript/AllInOne/examples/3-tsconfig/01.ts
+++ typescript/AllInOne/examples/3-tsconfig/01.ts
@@ -1,3 +1,12 @@
+const fs = require('fs')
+const lodash = require('lodash')
+
 let a = new Set()
 console.log(a.add(1))
+const fi = fs.readFileSync('D:\\project\\github\\itxcr.github.io\\typescript\\AllInOne\\examples\\2-深入\\01.ts')
+console.log(lodash)
+console.log(fi.toString())
+class Car {
+}
+
 export {}
